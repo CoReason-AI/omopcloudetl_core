2c69ef39077077189ce535ec0b953e8afff7e987
Gowtham Rao <gowthamrao@gmail.com>
2025-10-26 10:50:23 -0400

    d

--- src/omopcloudetl_core/main.py
+++ src/omopcloudetl_core/main.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2025 Scientific Informatics, LLC
+# Copyright (c) 2025 Scientific Informatics, LLC.
 #
 # This software is proprietary and dual-licensed.
 # Licensed under the Prosperity Public License 3.0 (the "License").
